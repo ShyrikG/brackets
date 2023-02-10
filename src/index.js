7ebaf113f6ae53d8e523029b7894df3b72af2cfd
Alexandr Hapon <g_shyrik@ukr.net>
2023-02-10 20:51:22 +0200

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let conf = Object.fromEntries(bracketsConfig); 
+  let opened = [];
+  for (let key in conf) {
+    opened.push(key);
+  };
+    let arr = [];
+
+    for (let i = 0; i < str.length; i++) {
+      let elem = str[i];        
+      
+
+      if (arr.length === 0){  
+        if (opened.includes(elem)) {
+          arr.push(elem);
+        } else {
+          return false;
+        }
+      } else {
+
+        let previous = arr[arr.length - 1];
+        if (opened.includes(elem)) {
+          if ((conf[elem] === elem)&&(conf[previous] === elem)) {
+            arr.pop();
+          } else {
+            arr.push(elem);
+          }                           
+        } else {
+          if (conf[previous] === elem) {
+            arr.pop();
+          } else {
+            return false;
+          }
+        }
+      }        
+    }
+
+      return (arr.length === 0) ? true : false; 
 }
